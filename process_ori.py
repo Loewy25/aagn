3193e2c1c7095fd9bd235e5a74aa69659afb8c7c
Loewy25 <113399216+Loewy25@users.noreply.github.com>
2025-03-01 22:07:58 -0600

    Create process_ori.py

---
+++ process_ori.py
@@ -0,0 +1,44 @@
+import glob
+import os
+import argparse
+
+import ants
+from ants.utils.bias_correction import n4_bias_field_correction
+from tqdm import tqdm
+from deepbrain import Extractor
+import nibabel as nib
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--i", type=str, help="Input directory")
+    parser.add_argument("--o", type=str, help="Output directory")
+    opt = parser.parse_args()
+    print(opt)
+    
+    if not os.path.exists(opt.o):
+        os.makedirs(opt.o)
+    
+    fixed = ants.image_read('MNI152_T1_2mm_Brain.nii.gz', reorient=True)
+    ext = Extractor()
+    
+    for filename in tqdm(glob.glob("{}/*.nii".format(opt.i))):
+        orig = ants.image_read(filename, reorient=True)
+
+        orig = n4_bias_field_correction(orig)
+
+        # brain extraction
+        img = orig.numpy()
+        prob = ext.run(img) 
+        mask = prob < 0.5
+        img[mask] = 0
+        img = ants.copy_image_info(orig, ants.from_numpy(img))
+
+        # registration
+        mytx = ants.registration(fixed=fixed, moving=img, type_of_transform='SyN')
+        img = mytx['warpedmovout']
+
+        nib.save(ants.from_numpy(img), opt.o + "/" + filename.split("/")[-1])
+
+    
+    print("Processing done :)")
